b7af4ef6bc564ac4f98674f9156a3065c06a466a
PLGRZFIL <filip.grzebalski@carlsberg.com>
2023-01-02 15:47:40 +0100

    Added reading Windows User for win_user Variable + plus printing it, when Parameters.py is run as main script

--- Parameters.py
+++ Parameters.py
@@ -1,10 +1,16 @@
+from os import getlogin
+
 # both lists need to be same length
 bands = [0.2, 0.4, 0.55, 0.68, 0.78, 0.87, 0.95, 0.98, 1.01]
 cuts = [95, 95, 95, 95, 95, 95, 95, 95, 95, 95]
-win_user = 'PLKOPCKA'
+win_user = getlogin()
 max_solve = 7200 # max time solve for the model
 min_gap = 0.4 # min gap value for the model
 scenario_name = 'Test_SS_KEG' # name of the Scenario outputs in the Database Table
 model_name = 'CUAL' # name of the model to choose from the CUAL folder
 resolve = False  # if gap reached in max_solve time is lower than defined min_gap then split band into half
 ISC_path = 'Transportation Lane Products Results.csv' # path for the download directory
+
+
+if __name__ == '__main__':
+    print(win_user)
